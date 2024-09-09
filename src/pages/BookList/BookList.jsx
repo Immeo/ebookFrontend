20d682ad29cb701f1c993f03ca6b10adb6ec3bae
immeo <wayfarer245@gmail.com>
2024-09-10 01:26:34 +0300

    10-added a custom hook  useGetBooksList

--- src/pages/BookList/BookList.jsx
+++ src/pages/BookList/BookList.jsx
@@ -1,32 +1,9 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import renderStarsIcon from "../../assets/icons/renderStarsIcon;";
+import { useGetBooksList } from "../../hooks/api/useGetBoks";
 import BookCard from "./BookCard/BookCard";
 
 function BookList() {
-  const [books, setBooks] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    getBooksList();
-  }, []); // Убрать setBooks из зависимостей
-
-  const getBooksList = async () => {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      const response = await axios.get(
-        `http://192.168.0.90:8000/api/v1/bookslist/`,
-        {
-          withCredentials: false,
-        },
-      );
-      console.log(response);
-      setBooks(response.data);
-    } catch (error) {
-      console.error("Error fetching books:", error);
-      setError(error.message);
-    }
-  };
+  const { books, error } = useGetBooksList();
 
   const sumRating = (rating) => {
     if (!rating || !rating.length) return 0;
